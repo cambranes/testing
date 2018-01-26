d3d814c04e3947fb76c8c4e3010d4699dbca28e3
carlospq17 <carlospq_17@hotmail.com>
2018-01-26 12:53:59 -0600

    QA
    DONE

--- ElenaYEsclavos-Sol1.c
+++ ElenaYEsclavos-Sol1.c
@@ -34,5 +34,4 @@ int main(void) {
 		printf(" digitos");
 	return 0;
 }
-
-*/QA	Entrada 		Salida			Resultado	0				0				1	12345			5				5	43				2				2	2				1				1	*/
+/*QA	Entrada 		Salida			Resultado	0				0				1	12345			5				5	43				2				2	2				1				1	*/
