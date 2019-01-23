8e425a0d33e19dccc945649a7b463e603a8978d1
RaulRivadeneyra <rivadeneyraburgos.r@gmail.com>
2019-01-23 08:05:03 -0600

    Renamed and edited the autor

---
+++ EQ1.Raul-Rivadeneyra.c
@@ -0,0 +1,18 @@
+/*
+Autor:Raul Rivadeneyra 23/01/2019
+Entradas:
+Salidas:
+Procedimiento general:
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	/*Entradas*/
+	
+	/*Procedimiento*/
+
+	/*Salidas*/
+	return 0;
+}
+
