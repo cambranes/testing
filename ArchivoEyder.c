dfd47ebceae72ab771df6f8a6a944536b34f1981
Eyder A. Concha <eyderacm@gmail.com>
2019-01-23 08:26:15 -0600

    Update and rename Template.c to ArchivoEyder.c

---
+++ ArchivoEyder.c
@@ -0,0 +1,18 @@
+/*
+Autor:Eyder Concha 26/Enero/17
+Entradas:
+Salidas:
+Procedimiento general:
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	/*Entradas*/
+	
+	/*Procedimiento*/
+
+	/*Salidas*/
+	return 0;
+}
+
