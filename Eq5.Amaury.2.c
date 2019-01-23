706bec8928fc16f540c0962651bb44b2dd790e10
amc <46652567+amaurymc@users.noreply.github.com>
2019-01-23 08:36:53 -0600

    Create Eq5.Amaury.2.c

---
+++ Eq5.Amaury.2.c
@@ -0,0 +1,16 @@
+/*
+Autor: aaaaaaaaaaaaa
+Entradas:
+Salidas:
+Procedimiento general:
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	/*Entradas*/
+	
+	/*Procedimiento*/
+	/*Salidas*/
+	return 0;
+}
