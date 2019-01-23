6e2f70f491360b03681e01ac908a6735b35f3718
Jorge Alberto Chí León <46463280+JorchCanelo@users.noreply.github.com>
2019-01-23 08:01:39 -0600

    Update Template.c

--- Template.c
+++ Template.c
@@ -1,5 +1,5 @@
 /*
-Autor:Edgar Cambranes 26/Enero/17
+Autor:Jorge Chí 23/Enero/19
 Entradas:
 Salidas:
 Procedimiento general:
@@ -10,7 +10,8 @@ Procedimiento general:
 int main(int argc, char *argv[]) {
 	/*Entradas*/
 	
-	/*Procedimiento*/
+	/*Procedimiento*/
+
 	/*Salidas*/
 	return 0;
 }
