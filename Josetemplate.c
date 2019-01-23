ed1bd9e4f54bcfa221b612c21165bce130ade063
fadedphoenix7 <43895609+fadedphoenix7@users.noreply.github.com>
2019-01-23 08:22:28 -0600

    Create Josetemplate.c

---
+++ Josetemplate.c
@@ -0,0 +1,16 @@
+/*
+Autor:Jose Eduardo Mendez Verdejo 23/Enero/19
+Entradas:
+Salidas:
+Procedimiento general:
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	/*Entradas*/
+	
+	/*Procedimiento*/
+	/*Salidas*/
+	return 0;
+}
