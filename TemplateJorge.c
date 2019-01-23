786c6bd35dfa6a37cbe8bed04b09c1f186914361
Jorge Alberto Chí León <46463280+JorchCanelo@users.noreply.github.com>
2019-01-23 08:17:49 -0600

    Update TemplateJorge.c

--- TemplateJorge.c
+++ TemplateJorge.c
@@ -1,1 +1,17 @@
+/*
+Autor:Jorge Chí 23/Enero/19
+Entradas:
+Salidas:
+Procedimiento general:
+*/
 
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	/*Entradas*/
+	
+	/*Procedimiento*/
+
+	/*Salidas*/
+	return 0;
+}
