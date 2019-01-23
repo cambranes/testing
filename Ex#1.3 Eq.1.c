ac3b5e20bc16082f69605514fac018c39d80e036
JimmyOjeda <46965072+JimmyOjeda@users.noreply.github.com>
2019-01-23 08:35:19 -0600

    Update Ex#1.3 Eq.1.c

---
+++ Ex#1.3 Eq.1.c
@@ -0,0 +1,17 @@
+/*
+Autor:Jimmy Ojeda 22/Enero/19
+Entradas:
+Salidas:
+Procedimiento general:
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	/*Entradas*/
+	
+	/*Procedimiento*/
+
+	/*Salidas*/
+	return 0;
+}
